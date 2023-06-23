d9ad7ae8c24a61c9736a7d0ef4c3dc49450a0a31
nieeyoung <52353487+nieeyoung@users.noreply.github.com>
2023-06-23 04:54:01 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/signup.html");
+    res.sendFile(__dirname, 'public' + "/signup.html");
 });
 
 app.post("/", function(req, res){
